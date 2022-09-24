3a49c1e661e83b532c2510aea17b41ec6e9ae7bf
Cristian Venegas <cristian.venegas@materoceano.com>
2022-09-24 00:35:58 -0300

    fix : footer position

--- src/components/MyFooter.jsx
+++ src/components/MyFooter.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const MyFooter = () => {
   return (
-    <div className='w-full h-[64px] bg-pink-400 '>MyFooter</div>
+    <div className='w-full h-[64px] bg-pink-400 text-white flex justify-center items-center'>Happy Cupcake 2022</div>
   )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,57 +3,70 @@ import { Card } from "flowbite-react";
 const Home = () => {
   return (
     <>
-      <div className="bg-white dark:bg-gray-900 sm:h-[calc(100vh-128px)] grid gap-x-8 gap-y-4 grid-cols-3 ">
+      <div className=" md:h-auto xl:h-[calc(100vh-128px)] flex flex-row flex-wrap mb-auto  gap-6 p-20 justify-center">
+        <div className="tarjeta w-60 h-180" >
+        <Card imgSrc="https://wallpaperaccess.com/full/1986114.jpg">
+              <h5 className=" text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                Noteworthy technology acquisitions 2021
+              </h5>
+              <p className="font-normal text-gray-700 dark:text-gray-400">
+                Here are the biggest enterprise technology acquisitions of
+              </p>
+            </Card>
+        </div>
+        <div className="tarjeta w-60 h-180" >
+        <Card imgSrc="https://wallpaperaccess.com/full/4681378.jpg">
+              <h5 className=" text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                Noteworthy technology acquisitions 2021
+              </h5>
+              <p className="font-normal text-gray-700 dark:text-gray-400">
+                Here are the biggest enterprise technology acquisitions of
+              </p>
+            </Card>
+        </div>
+        <div className="tarjeta w-60 h-180" >
+        <Card imgSrc="https://wallpaperaccess.com/full/4681361.jpg">
+              <h5 className=" text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                Noteworthy technology acquisitions 2021
+              </h5>
+              <p className="font-normal text-gray-700 dark:text-gray-400">
+                Here are the biggest enterprise technology acquisitions of
+              </p>
+            </Card>
+        </div>
+        <div className="tarjeta w-60 h-180" >
+        <Card imgSrc="https://wallpaperaccess.com/full/4681357.jpg">
+              <h5 className=" text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                Noteworthy technology acquisitions 2021
+              </h5>
+              <p className="font-normal text-gray-700 dark:text-gray-400">
+                Here are the biggest enterprise technology acquisitions of
+              </p>
+            </Card>
+        </div>
+        <div className="tarjeta w-60 h-180" >
+        <Card imgSrc="https://wallpaperaccess.com/full/1986146.jpg">
+              <h5 className=" text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                Noteworthy technology acquisitions 2021
+              </h5>
+              <p className="font-normal text-gray-700 dark:text-gray-400">
+                Here are the biggest enterprise technology acquisitions of
+              </p>
+            </Card>
+        </div>
+        <div className="tarjeta w-60 h-180" >
+        <Card imgSrc="https://wallpaperaccess.com/full/568602.jpg">
+              <h5 className=" text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                Noteworthy technology acquisitions 2021
+              </h5>
+              <p className="font-normal text-gray-700 dark:text-gray-400">
+                Here are the biggest enterprise technology acquisitions of
+              </p>
+            </Card>
+        </div>
         
-        
-        <Card imgSrc="https://flowbite.com/docs/images/blog/image-1.jpg">
-              <h5 className=" text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                Noteworthy technology acquisitions 2021
-              </h5>
-              <p className="font-normal text-gray-700 dark:text-gray-400">
-                Here are the biggest enterprise technology acquisitions of
-              </p>
-            </Card>
-            <Card imgSrc="https://flowbite.com/docs/images/blog/image-1.jpg">
-              <h5 className=" text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                Noteworthy technology acquisitions 2021
-              </h5>
-              <p className="font-normal text-gray-700 dark:text-gray-400">
-                Here are the biggest enterprise technology acquisitions of
-              </p>
-            </Card>
-            <Card imgSrc="https://flowbite.com/docs/images/blog/image-1.jpg">
-              <h5 className=" text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                Noteworthy technology acquisitions 2021
-              </h5>
-              <p className="font-normal text-gray-700 dark:text-gray-400">
-                Here are the biggest enterprise technology acquisitions of
-              </p>
-            </Card>
-            <Card imgSrc="https://flowbite.com/docs/images/blog/image-1.jpg">
-              <h5 className=" text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                Noteworthy technology acquisitions 2021
-              </h5>
-              <p className="font-normal text-gray-700 dark:text-gray-400">
-                Here are the biggest enterprise technology acquisitions of
-              </p>
-            </Card>
-            <Card imgSrc="https://flowbite.com/docs/images/blog/image-1.jpg">
-              <h5 className=" text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                Noteworthy technology acquisitions 2021
-              </h5>
-              <p className="font-normal text-gray-700 dark:text-gray-400">
-                Here are the biggest enterprise technology acquisitions of
-              </p>
-            </Card>
-            <Card imgSrc="https://flowbite.com/docs/images/blog/image-1.jpg">
-              <h5 className=" text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                Noteworthy technology acquisitions 2021
-              </h5>
-              <p className="font-normal text-gray-700 dark:text-gray-400">
-                Here are the biggest enterprise technology acquisitions of
-              </p>
-            </Card>
+       
+            
         
         </div>
       
